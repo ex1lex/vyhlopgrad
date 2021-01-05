419b1f6bf3037399f9af2147e687ab374378c6e8
Obryadin Alexey <obryadin.alexey@gmail.com>
2021-01-05 15:16:41 +0300

    refactor: бэм структура

--- scripts.js
+++ scripts.js
@@ -2,9 +2,12 @@ const header = document.querySelector(".header__top-line");
 const logo = document.querySelector(".header__logo_type_vertical");
 const text = document.querySelector(".header__text");
 
-logo.classList.add("header__logo_show");
+window.addEventListener("load", function (event) {
+  console.log("All resources finished loading!");
+  logo.classList.add("header__logo_show");
 
-text.classList.add("header__text_show");
+  text.classList.add("header__text_show");
+});
 
 window.addEventListener("scroll", () => {
   if (pageYOffset >= 800) {
